b583009a65bf4748a2063d04efa7fc0b9bc80b57
chris <chris.dinozzi@gmail.com>
2024-05-12 17:27:44 +0100

    more things

--- app.js
+++ app.js
@@ -19,38 +19,11 @@ function setOutput(id,value){
     }
 }
 
-function hex2bin(hex){//https://stackoverflow.com/a/64235212
-    hex = hex.replace("0x", "").toLowerCase();
-    var out = "";
-    for(var c of hex) {
-        switch(c) {
-            case '0': out += "0000"; break;
-            case '1': out += "0001"; break;
-            case '2': out += "0010"; break;
-            case '3': out += "0011"; break;
-            case '4': out += "0100"; break;
-            case '5': out += "0101"; break;
-            case '6': out += "0110"; break;
-            case '7': out += "0111"; break;
-            case '8': out += "1000"; break;
-            case '9': out += "1001"; break;
-            case 'a': out += "1010"; break;
-            case 'b': out += "1011"; break;
-            case 'c': out += "1100"; break;
-            case 'd': out += "1101"; break;
-            case 'e': out += "1110"; break;
-            case 'f': out += "1111"; break;
-            default: return "";
-        }
-    }
-
-    return out;
-} 
 ///////////////////
 //DATA LINK LAYER//
 ///////////////////
 function getStart(input){
-    console.log("Start: "+input)
+    //console.log("Start: "+input)
     if (input=="0564"){
         state="valid"
     } else{
@@ -60,15 +33,15 @@ function getStart(input){
 }
 
 function getLength(input){
-    console.log("Length: "+input)
+    //console.log("Length: "+input)
     as_int = Number(input)
     return "0x"+input +`(${as_int})`
 }
 
 function getDirection(input){
-    console.log("Control (direction): "+input)
+    //console.log("Control (direction): "+input)
     as_int = Number("0x"+input)
-    console.log(as_int>>7 & 0x1)
+    //console.log(as_int>>7 & 0x1)
     if ((as_int>>7 & 0x1) == 1){
         return("From Master to Outstation")
     } else{
@@ -77,7 +50,7 @@ function getDirection(input){
 }
 
 function getPrimaryMessage(input){
-    console.log("Control (PRM): "+input)
+    //console.log("Control (PRM): "+input)
     as_int = Number("0x"+input)
     if ((as_int>>6 & 0x01) == 1){
         return("Primary to Secondary")
@@ -94,8 +67,8 @@ function getLinkFunctionCode(input){
     } else{
         prm=0
     }
-    console.log("Link Function Code: "+input.substring(1))
-    console.log("PRM: "+prm)
+    //console.log("Link Function Code: "+input.substring(1))
+    //console.log("PRM: "+prm)
     if (prm==1){
         switch (input.substring(1)){
             case "0":
@@ -136,21 +109,21 @@ function getLinkFunctionCode(input){
 }
 
 function getDestination(input){
-    console.log("Destination: "+input)
+    //console.log("Destination: "+input)
     //swap bytes around
     input = input.substring(2) +input.substring(0,2)
     return Number("0x"+input)
 }
 
 function getSource(input){
-    console.log("Source: "+input)
+    //console.log("Source: "+input)
     //swap bytes around
     input = input.substring(2) +input.substring(0,2)
     return Number("0x"+input)
 }
 
 function getCRC(input){
-    console.log("CRC: "+input)
+    //console.log("CRC: "+input)
     //swap bytes around
     input = input.substring(2) +input.substring(0,2)
     return "0x"+input
@@ -158,22 +131,67 @@ function getCRC(input){
 
 function handleDataLinkLayer(input){
     //056405C001000004E921  <-- example input
+    setOutput("data-link-data",input)
     var start = input.substring(0,4)
     var length = input.substring(4,6)
     var control = input.substring(6,8)
     var destination = input.substring(8,12)
     var source = input.substring(12,16)
     var crc = input.substring(16,20)
-    console.log("Data Link Layer: "+input)
+    //console.log("Data Link Layer: "+input)
     
     setOutput("start",getStart(start))
     setOutput("length",getLength(length))
     setOutput("direction",getDirection(control))
     setOutput("prm",getPrimaryMessage(control))
+    //TODO: add something for DFC
     setOutput("link_function_code",getLinkFunctionCode(control))
     setOutput("destination",getDestination(destination))
     setOutput("source",getSource(source))
     setOutput("crc",getCRC(crc))
+}
+///////////////////
+//TRANSPORT LAYER//
+///////////////////
+function getFIN(input){
+    as_int = Number("0x"+input)
+    if ((as_int>>7 & 0x1) == 1){
+        return "1"
+    } else{
+        return "0"
+    }
+}
+
+function getFIR(input){
+    as_int = Number("0x"+input)
+    if ((as_int>>6 & 0x01) == 1){
+        return "1"
+    } else{
+        return "0"
+    }
+}
+
+function getSeq(input){
+    as_int = Number("0x"+input)
+    return as_int<<2
+}
+
+function handleTransportLayer(input){
+    setOutput("transport-layer-data",input)
+    setOutput("fin",getFIN(input))
+    setOutput("fir",getFIR(input))
+    setOutput("seq",getSeq(input))
+
+}
+
+
+///////////////////////
+//APPLICATION HEADER//
+//////////////////////
+function handleApplicationHeader(input){
+    //056414F3010000040A3BC0 C3 01 3C 02 06 3C 03 06 3C 04 06 3C 01 06 9A 12
+    setOutput("application-header-data",input)
+    console.log("Application Header: "+ input)
 }
 
 function main(input){
@@ -183,5 +201,6 @@ function main(input){
     }
 
     handleDataLinkLayer(input.substring(0,20))
-
+    handleTransportLayer(input.substring(20,22))
+    handleApplicationHeader(input.substring(22,28))
 }
